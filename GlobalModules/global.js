7a8c816ddd3416fed1695af5d0808c2506a2a660
niawjunior <pasupol@dome.cloud>
2018-03-08 11:41:40 +0700

    add javascript standard style

--- GlobalModules/global.js
+++ GlobalModules/global.js
@@ -1,5 +1,5 @@
 function printSomething () {
-    console.log('I am global')
+  console.log('I am global')
   console.log('I am here-> ', `${__dirname} +  And my name is->, ${__filename}`)
 }
 setTimeout(printSomething, 1000)
